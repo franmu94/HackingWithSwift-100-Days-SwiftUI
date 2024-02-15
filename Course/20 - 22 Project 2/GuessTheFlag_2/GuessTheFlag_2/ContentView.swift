72774fbfe41803973580abe61c8cdebcb737c586
franmu94 <fmalourarte@gmail.com>
2024-02-15 21:33:28 +0100

    punto antes de intenatarlo

---
+++ Course/20 - 22 Project 2/GuessTheFlag_2/GuessTheFlag_2/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  GuessTheFlag_2
+//
+//  Created by Fran Malo on 15/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
+
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    var body: some View {
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
+            }
+
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Course/20 - 22 Project 2/GuessTheFlag_2/GuessTheFlag_2/GuessTheFlag_2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag_2App.swift
+//  GuessTheFlag_2
+//
+//  Created by Fran Malo on 15/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
