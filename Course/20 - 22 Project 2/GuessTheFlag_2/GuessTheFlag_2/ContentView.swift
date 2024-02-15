e0e72462cf972a371490e86142e9ebdc7e56dcab
franmu94 <fmalourarte@gmail.com>
2024-02-15 23:58:08 +0100

    punto antes de intenatarlo

--- Course/20 - 22 Project 2/GuessTheFlag_2/GuessTheFlag_2/ContentView.swift
+++ Course/20 - 22 Project 2/GuessTheFlag_2/GuessTheFlag_2/ContentView.swift
@@ -16,23 +16,51 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     var body: some View {
         ZStack{
-            Color.blue
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
-            VStack(spacing: 30) {
+            
+            VStack(spacing: 15) {
                 VStack {
+                    Text("Guess the Flag")
+                        .font(.largeTitle.bold())
+                        .foregroundStyle(.white)
                     Text("Tap the flag of")
                         .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+
                     Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
+                        .foregroundStyle(.secondary)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 ForEach(0..<3) { number in
                     Button {
                         flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(color: Color.yellow, radius: 5)
+                         
                     }
                 }
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
             }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 20)
+            
+            .background(.regularMaterial)
+            .clipShape(.rect(cornerRadius: 20))
+            .padding()
+
+
+
+            
+            
+            
 
         }
         .alert(scoreTitle, isPresented: $showingScore) {
