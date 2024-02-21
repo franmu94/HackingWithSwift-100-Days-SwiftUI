155a6b78bfe59d0f311ae015913cb492f4683b2a
franmu94 <fmalourarte@gmail.com>
2024-02-21 19:05:32 +0100

    cambios dia 22, 4

--- Course/23 -24 Project 3/ViewsAndModif/ViewsAndModif/ContentView.swift
+++ Course/23 -24 Project 3/ViewsAndModif/ViewsAndModif/ContentView.swift
@@ -7,28 +7,52 @@
 //
 
 import SwiftUI
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
 
-struct ContentView: View {
-    struct CapsuleText: View {
-        var text: String
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
 
-        var body: some View {
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
             Text(text)
-                .font(.largeTitle)
-                .padding()
-                .background(.blue)
-                .clipShape(.capsule)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
         }
     }
+}
+
+
+
+struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            CapsuleText(text: "First")
-                .foregroundStyle(.white)
-
-            CapsuleText(text: "Second")
-                .foregroundStyle(.red)
-
+        ZStack {
+            Color.blue
+            Image(systemName: "sun.max.fill")
+                .foregroundStyle(.yellow)
+                .t
         }
+        .frame(width: .infinity, height: .infinity)
     }
     
 }
